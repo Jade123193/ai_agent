e389e0f3945a4bf7bd82758a6b3a6121fed8e9bc
Jade T Stanley <170278496+Jade123193@users.noreply.github.com>
2025-06-15 00:39:18 -0400

    Removed hardcoded prompt and replaced with sys.argv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,10 +9,12 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
-
+if len(sys.argv)< 2:
+    print("error message and exit the program with exit code 1.")
+    sys.exit(1)
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents=['Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.']
+    contents=sys.argv[1]
 )
 print(response.text)
 print("Prompt tokens:", response.usage_metadata.prompt_token_count)
