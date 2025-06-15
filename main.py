815f41e986b061b702057c64dadd3c7a7a32fa37
Jade T Stanley <170278496+Jade123193@users.noreply.github.com>
2025-06-15 18:11:31 -0400

    Added the command line argument --verbose

--- main.py
+++ main.py
@@ -12,11 +12,23 @@ client = genai.Client(api_key=api_key)
 if len(sys.argv)< 2:
     print("error message and exit the program with exit code 1.")
     sys.exit(1)
+
+contents = sys.argv[1]
+
+if "--verbose" in sys.argv:
+    verbose = True
+else:
+    verbose = False
+if verbose:
+    print("User prompt:", contents) 
+
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents=sys.argv[1]
+    contents=contents
 )
 print(response.text)
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+if verbose:
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
